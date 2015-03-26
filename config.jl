f430e37bec24ed0d0585f291a42b9ce905134df8
ST John <stj26@cam.ac.uk>
2015-03-26 16:38:48 +0000

    Add inner constructor to Configuration type enforcing sizes of types, pos, force to be consistent

--- config.jl
+++ config.jl
@@ -4,10 +4,15 @@ immutable Configuration
     types::Vector{Symbol}
     box
     pos
-    forces
+    force
+    function Configuration(types::Vector{Symbol}, box, pos, force)
+        (length(types) == size(pos, 2) == size(force, 2)) || error("number of sites does not match")
+        (size(pos) == size(force)) || error("size of frames does not match")
+        new(types, box, pos, force)
+    end
 end
 
-Configuration(types::Vector{Symbol}, pos, forces) = Configuration(types, nothing, pos, forces)
+Configuration(types::Vector{Symbol}, pos, force) = Configuration(types, nothing, pos, force)
 
 wrapvec(L, v) = (v + L/2) .% L - L/2
 wrapvec(L::Vector, v::Matrix) = (v .+ L'/2) .% L' .- L'/2
